d6cfc69b130163beb08a677f0e6a36897c00749e
pvirie <p.virie@gmail.com>
2019-09-07 17:56:42 +0700

    add files

---
+++ src/main.py
@@ -0,0 +1,6 @@
+import model
+import world
+
+
+if __name__ == '__main__':
+    pass
---
+++ src/model.py
@@ -0,0 +1,15 @@
+import os
+
+
+class Intellectual_being:
+    def __init__(self):
+        pass
+
+    def set_goal(self):
+        pass
+
+    def feed_external(self):
+        pass
+
+    def update(self, world):
+        pass
