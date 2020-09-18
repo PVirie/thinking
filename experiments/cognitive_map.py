27a2673835188fa4c892b8b1088d833f20c3ca0e
Chatavut Viriyasuthee <p.virie@gmail.com>
2020-09-18 20:44:31 +0700

    paths do not end.

--- experiments/cognitive_map.py
+++ experiments/cognitive_map.py
@@ -11,8 +11,8 @@ def build_estimate_and_seed_graph(graph, seed):
 
 
 def build_layer_basic(graph):
-    encoder = np.arange(graph.shape[0])
-    decoder = np.arange(graph.shape[0])
+    encoder = np.arange(graph.shape[0], dtype=np.int32)
+    decoder = np.arange(graph.shape[0], dtype=np.int32)
     graph = graph
     top, estimate = build_layer_strategy_A(graph)
 
@@ -20,19 +20,36 @@ def build_layer_basic(graph):
 
 
 def build_layer_strategy_A(graph):
-	print("building another layer with strategy A.")
-	
+
     out_going, in_coming = node_degrees(graph)
     degrees = out_going + in_coming
-    pivots = np.arange(graph.shape[0])[degrees > np.mean(degrees)/2]
+    pivots = np.arange(graph.shape[0])[degrees > np.mean(degrees)]
+    print("building another layer with strategy A:", pivots.shape[0])
+
+    if pivots.shape[0] == 0:
+        # initialize estimand
+        inf = graph.shape[0] * 2
+        estimand = np.ones_like(graph) * inf
+
+        step = 1000
+        step_size = inf/1000
+        trace = []
+        for i in range(step):
+            path = random_path(graph, random.randint(0, graph.shape[0]-1), graph.shape[0]-1)
+            for v in path:
+                estimand[trace , v] = estimand[trace , v] - step_size    
+                trace.append(v)
+            trace.clear()
+        return None, estimand
+
     decoder = pivots
 
     # initialize pivot graph
     pivot_graph = np.zeros([pivots.shape[0], pivots.shape[0]], dtype=np.float32)
 
     # initialize encoder
-    encoder = np.ones(graph.shape[0]) * -1
-    encoder[pivots] = np.arange(pivots.shape[0]) # encoder is not complete 
+    encoder = np.ones(graph.shape[0], dtype=np.int32) * -1
+    encoder[pivots] = np.arange(pivots.shape[0], dtype=np.int32) # encoder is not complete 
 
     # initialize estimand
     inf = graph.shape[0] * 2
@@ -42,24 +59,25 @@ def build_layer_strategy_A(graph):
     step_size = inf/1000
     trace = []
     for i in range(step):
-    	path = random_path(graph, random.randint(graph.shape[0]), graph.shape[0])
-    	pv_index = None
-    	for v in path:
-    		estimand[trace , v] = estimand[trace , v] - step_size    		
-    		# check if one of the pivots
-    		v_index = encoder[v]
-    		if v_index >= 0:
-    			# link pivot graph
-    			if pv_index is not None:
-    				pivot_graph[pv_index , v_index] = 1 # this is for 0, 1 graph only
-    			pv_index = v_index
-    			# also if we found a pivot, clear trace
-	    		trace.clear()
+        path = random_path(graph, random.randint(0, graph.shape[0]-1), graph.shape[0]-1)
+        pv_index = None
+        for v in path:
+            estimand[trace , v] = estimand[trace , v] - step_size           
+            # check if one of the pivots
+            v_index = encoder[v]
+            if v_index >= 0:
+                # link pivot graph
+                if pv_index is not None:
+                    pivot_graph[pv_index, v_index] = 1 # this is for 0, 1 graph only
+                pv_index = v_index
+                # also if we found a pivot, clear trace
+                trace.clear()
 
-    		trace.append(v)
+            trace.append(v)
+        trace.clear()
 
     est_pivots = estimand[pivots, :] + np.transpose(estimand)[pivots, :] 
-	encoder = np.argmin(est_pivots, axis=0)
+    encoder = np.argmin(est_pivots, axis=0)
 
     top, estimate = build_layer_strategy_A(pivot_graph)
     return Layer(encoder, decoder, pivot_graph, estimate, top), estimand
@@ -72,7 +90,7 @@ class Layer:
         self.graph = graph
         self.estimate = estimate
         self.top = top
-        self.indices = np.arange(self.graph.shape[0])
+        self.indices = np.arange(self.graph.shape[0], dtype=np.int32)
 
     def encode(self, s):
         return self.encoder[s]
@@ -90,7 +108,7 @@ class Layer:
             if c == t:
                 break
             unvisited[c] = False
-            neighbors = np.logical_and((self.graph[c, :] > 0), unvisited).astype(np.float32)
+            neighbors = np.logical_and((self.graph[c, :] > 0), unvisited)
             c = self.indices[neighbors][np.argmin(self.graph[c, neighbors] + self.estimate[neighbors, t])]
             yield c
 
@@ -121,7 +139,7 @@ class Cognitive_map:
     def __init__(self):
         pass
 
-    def __build_hierarchy(self, graph):
+    def build_hierarchy(self, graph):
         self.base_hierarchy, _ = build_layer_basic(graph)
 
     def find_path(self, s, t):
@@ -130,7 +148,8 @@ class Cognitive_map:
 
 
 if __name__ == '__main__':
-    # cognitive_map = Cognitive_map()
-
-    # g = random_graph(20, 0.3)
-    print(np.where(np.arange(10) == 9))
+    
+    g = random_graph(20, 0.3)
+    cognitive_map = Cognitive_map()
+    cognitive_map.build_hierarchy(g)
+    cognitive_map.find_path(0, g.shape[0]-1)
