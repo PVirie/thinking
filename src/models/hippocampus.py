cded78d3bc5f5664c9f8a30ae82235b5b3574de4
pvirie <p.virie@gmail.com>
2021-09-22 18:34:54 +0700

    tested hippocampus module

--- src/models/hippocampus.py
+++ src/models/hippocampus.py
@@ -13,19 +13,24 @@ class Hippocampus:
 
     def pincer_inference(self, neighbor_model, estimate_model, s, t):
         s_indices, s_prop = self.resolve_address(s)
-        t_indices, t_prop = self.resolve_address(t)
+        t_indices, t_prop = self.resolve_address(t, s_indices)
         hippocampus_prop = np.power(self.diminishing_factor, t_indices - s_indices) * s_prop * t_prop
-        hippocampus_rep = self.access_memory(s_indices + 1)
+        t_prop[s_indices >= self.h_size] = 0
+        # print(s_indices, t_indices, t_prop)
 
-        cortex_rep, cortex_prop = energy.Energy_model.pincer_inference(neighbor_model, estimate_model, s, t)
+        hippocampus_rep = self.access_memory(np.mod(s_indices + 1, self.h_size))
 
-        compare_results = hippocampus_prop > cortex_prop
-        results = np.where(compare_results, hippocampus_rep, cortex_rep)
-        return results, np.where(compare_results, hippocampus_prop, cortex_prop)
+        # cortex_rep, cortex_prop = energy.Energy_model.pincer_inference(neighbor_model, estimate_model, s, t)
+
+        # compare_results = hippocampus_prop > cortex_prop
+        # results = np.where(compare_results, hippocampus_rep, cortex_rep)
+        # return results, np.where(compare_results, hippocampus_prop, cortex_prop)
+
+        return hippocampus_rep, None
 
     def match(self, x):
         H_ = np.transpose(self.H)
-        prop = np.exp(- (np.linalg.norm(x, ord=2, axis=0, keepdims=True)**2 - 2 * np.matmul(H_, x)) + np.linalg.norm(H_, ord=2, axis=1, keepdims=True)**2)
+        prop = np.exp(- (np.linalg.norm(H_, ord=2, axis=1, keepdims=True)**2 - 2 * np.matmul(H_, x)) + np.linalg.norm(x, ord=2, axis=0, keepdims=True)**2)
         prop = prop / np.sum(prop, axis=0, keepdims=True)
         return prop
 
@@ -42,8 +47,13 @@ class Hippocampus:
     def __str__(self):
         return str((self.H > 0).astype(np.int32))
 
-    def resolve_address(self, x):
+    def resolve_address(self, x, start_indices=None):
         prop = self.match(x)
+        if start_indices is not None:
+            positions = np.reshape(np.arange(self.h_size), [-1, 1])
+            mask = (positions > np.reshape(start_indices, [1, -1])).astype(np.float32)
+            prop = prop * mask
+
         max_indices = np.argmax(prop, axis=0)
         supports = np.arange(x.shape[1])
         return max_indices, prop[max_indices, supports]
@@ -64,11 +74,13 @@ class Hippocampus:
 
 
 if __name__ == '__main__':
-    model = Hippocampus(8, 24)
-    model.incrementally_learn(np.array(np.eye(8)))
-    model.incrementally_learn(np.array(np.eye(8)))
+    model = Hippocampus(8, 4)
+    a = np.random.normal(0, 1, [8, 1])
+    b = np.random.normal(0, 1, [8, 1])
+    model.incrementally_learn(a)
+    model.incrementally_learn(b)
     print(model)
-    a_record = model.access_memory([10, 11])
+    a_record = model.access_memory([2, 3])
     print(a_record)
     addr = model.resolve_address(a_record)
     print(addr)
