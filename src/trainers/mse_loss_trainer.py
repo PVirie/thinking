7e085d562461c444e4e76a63696006b8ce167082
pvirie <p.virie@gmail.com>
2021-10-07 23:46:05 +0700

    updated mse_loss_trainer to use gaussian variational.

--- src/trainers/mse_loss_trainer.py
+++ src/trainers/mse_loss_trainer.py
@@ -5,7 +5,7 @@ from torch.utils.tensorboard import SummaryWriter
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
+import math
 import os
 import numpy as np
 
@@ -25,10 +25,6 @@ def compute_contranstive_loss(predictions, targets):
     return compute_mse_loss(predictions, targets) - compute_mse_loss(predictions, torch.mean(predictions, dim=1, keepdim=True))
 
 
-def compute_gausian_density_loss(predictions, targets, log_density):
-    return torch.mean(torch.square(predictions - targets) / 2) - torch.mean(log_density)
-
-
 def compute_gausian_variational_loss(predictions, targets):
     X = targets - predictions
     mu = torch.mean(X, dim=1)
@@ -36,10 +32,55 @@ def compute_gausian_variational_loss(predictions, targets):
     var = mean_sqr - mu * mu
     return torch.mean(mean_sqr - torch.log(var))
 
+
+log_2PI = math.log(2 * math.pi)
+
+
+def compute_log_gausian_density_loss(x, means, variances):
+    '''
+        x of shape [dims, batch]
+        means of shape [dims, batch]
+        variances of shape [dims, batch]
+    '''
+    dims = x.shape[0]
+    return torch.mean(torch.sum(torch.square(x - means) / (2 * variances), dim=0)
+                      + 0.5 * torch.sum(torch.log(variances), dim=0)
+                      + (0.5 * dims) * log_2PI
+                      )
+
+
+def compute_log_gausian_density_loss_against_pivots(x, masks, P, variances):
+    '''
+        x of shape [dims, length]
+        masks of shape [length, batch]
+        P of shape [dims, batch]
+        variances of shape [dims, batch]
+    '''
+    x = torch.unsqueeze(x, dim=2)
+    P = torch.unsqueeze(P, dim=1)
+
+    dims = x.shape[0]
+    return torch.mean(
+        torch.sum(
+            masks * torch.sum(torch.square(x - P) / (2 * torch.unsqueeze(variances, dim=1)), dim=0),
+            dim=0) / torch.sum(masks, dim=0)
+        + 0.5 * torch.sum(torch.log(variances), dim=0)
+        + (0.5 * dims) * log_2PI
+    )
+
+
+def generate_masks(pivots, length):
+    pos = torch.unsqueeze(torch.arange(0, length, dtype=torch.int32), dim=1)
+    pre_pivots = torch.roll(pivots, 1, 0)
+    pre_pivots[0] = -1
+    return torch.logical_and(pos > torch.unsqueeze(pre_pivots, dim=0), pos <= torch.unsqueeze(pivots, dim=0)).float()
+
 class Trainer:
 
-    def __init__(self, embedding_model, checkpoint_dir=None, save_every=None, num_epoch=100, lr=0.01, step_size=10, weight_decay=0.99, on_cpu=True):
+    def __init__(self, embedding_model, neighbor_variational_model, heuristic_variational_model, checkpoint_dir=None, save_every=None, num_epoch=100, lr=0.01, step_size=10, weight_decay=0.99, on_cpu=True):
         self.embedding_model = embedding_model
+        self.neighbor_variational_model = neighbor_variational_model
+        self.heuristic_variational_model = heuristic_variational_model
         self.checkpoint_dir = checkpoint_dir
         self.save_every = save_every
 
@@ -55,18 +96,24 @@ class Trainer:
 
         self.eval_mode = True
 
-    def incrementally_learn(self, path):
+    def incrementally_learn(self, path, pivots):
         self.embedding_model.train()
+        self.neighbor_variational_model.train()
+        self.heuristic_variational_model.train()
 
         path = torch.from_numpy(path) if type(path).__module__ == np.__name__ else path
+        pivots = torch.from_numpy(pivots) if type(pivots).__module__ == np.__name__ else pivots
+
         encoded_path = self.embedding_model.encode(path)
         V = encoded_path[:, :-1]
         H = encoded_path[:, 1:]
+        P = encoded_path[:, pivots]
 
-        # decoded_path = self.embedding_model.decode(encoded_path)
-        # reconstruction_loss = compute_mse_loss(encoded_path, decoded_path)
+        neighbor_var = self.neighbor_variational_model(V)
+        heuristic_var = self.heuristic_variational_model(P)
 
-        loss_values = compute_gausian_variational_loss(V, H)
+        loss_values = compute_log_gausian_density_loss(H, V, neighbor_var)
+        + compute_log_gausian_density_loss_against_pivots(encoded_path, generate_masks(pivots, path.shape[1]), P, heuristic_var)
         self.opt.zero_grad()
         loss_values.backward()
         self.opt.step()
@@ -74,7 +121,7 @@ class Trainer:
 
         return loss_values.detach().cpu().numpy(), self.step
 
-    def bootstrap(self, path):
+    def bootstrap(self, paths):
         clear_directory(self.checkpoint_dir)
 
         self.embedding_model.train()
@@ -84,12 +131,12 @@ class Trainer:
         sum_count = 0
 
         writer = SummaryWriter(os.path.join(self.checkpoint_dir, "logs"))
-        path = torch.from_numpy(path) if type(path).__module__ == np.__name__ else path
+        paths = torch.from_numpy(paths) if type(paths).__module__ == np.__name__ else paths
 
         while True:
 
             # Main training code
-            loss, iterations = self.incrementally_learn(path)
+            loss, iterations = self.incrementally_learn(paths)
             sum_loss = sum_loss + loss
             sum_count = sum_count + 1
 
@@ -113,6 +160,8 @@ class Trainer:
             latest = sorted(os.listdir(os.path.join(self.checkpoint_dir, "weights")))[-1]
             checkpoint = torch.load(os.path.join(self.checkpoint_dir, "weights", latest))
             self.embedding_model.load_state_dict(checkpoint['embedding_model'])
+            self.neighbor_variational_model.load_state_dict(checkpoint['neighbor_variational_model'])
+            self.heuristic_variational_model.load_state_dict(checkpoint['heuristic_variational_model'])
             self.opt.load_state_dict(checkpoint['opt'])
             self.scheduler.load_state_dict(checkpoint['scheduler'])
             self.step = checkpoint['step']
@@ -126,6 +175,8 @@ class Trainer:
 
         torch.save({
             'embedding_model': self.embedding_model.state_dict(),
+            'neighbor_variational_model': self.neighbor_variational_model.state_dict(),
+            'heuristic_variational_model': self.heuristic_variational_model.state_dict(),
             'opt': self.opt.state_dict(),
             'scheduler': self.scheduler.state_dict(),
             'step': self.step
@@ -133,4 +184,8 @@ class Trainer:
 
 
 if __name__ == '__main__':
-    pass
+    masks = generate_masks(torch.from_numpy(np.array([3, 5, 8])), 10)
+    print(masks)
+
+    print(compute_log_gausian_density_loss(torch.randn(4, 5), torch.randn(4, 5), torch.randn(4, 5)**2))
+    print(compute_log_gausian_density_loss_against_pivots(torch.randn(4, 10), masks, torch.randn(4, 3), torch.randn(4, 3)**2))
