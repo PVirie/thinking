bbf653716ea39df4f020e1f23bca4e2733a35620
pvirie <p.virie@gmail.com>
2022-03-27 13:21:30 +0700

    added representation base placeholder, gan trainer

---
+++ src/representations/mutual_knapsack.py
@@ -0,0 +1,16 @@
+import os
+import rep_base
+import numpy as np
+import sys
+
+
+class Model(rep_base.Model):
+
+    def __init__(self, knapsack_size, mutual_dims):
+        pass
+
+    def dims(self):
+        return 0
+
+    def dist(self, c):
+        return 0
---
+++ src/representations/rep_base.py
@@ -0,0 +1,12 @@
+
+
+class Model:
+
+    def __init__(self, dims):
+        pass
+
+    def dims(self):
+        return 0
+
+    def dist(self, c):
+        return 0
