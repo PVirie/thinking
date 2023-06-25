847b1edab49f65a60e0eda27ca6bc002ba10b6ee
Chatavut Viriyasuthee <p.virie@gmail.com>
2023-06-26 00:42:24 +0700

    step-by-step inference

--- src/network.py
+++ src/network.py
@@ -70,14 +70,14 @@ class Layer:
         cortex_rep, cortex_prop = await self.heuristics.consolidate(s, candidates, props, t)
         hippocampus_rep, hippocampus_prop = await self.hippocampus.infer(s, t)
 
-        # info = [(self.hippocampus.enhance(cortex_rep), cortex_prop), (self.hippocampus.enhance(hippocampus_rep), hippocampus_prop)]
+        sup_info = [(self.hippocampus.enhance(cortex_rep), cortex_prop), (self.hippocampus.enhance(hippocampus_rep), hippocampus_prop)]
 
         if pathway_bias < 0:
-            return hippocampus_rep
+            return hippocampus_rep, sup_info
         if pathway_bias > 0:
-            return cortex_rep
+            return cortex_rep, sup_info
 
-        return hippocampus_rep if hippocampus_prop > cortex_prop else cortex_rep
+        return hippocampus_rep if hippocampus_prop > cortex_prop else cortex_rep, sup_info
 
     async def find_path(self, c: Node, t: Node, hard_limit=20, pathway_bias=0):
         # pathway_bias < 0 : use hippocampus
@@ -85,22 +85,22 @@ class Layer:
 
         count_steps = 0
 
-        logger.info({
+        yield False, {
             "layer": self.name,
             "s": c,
             "t": t
-        })
+        }
 
         if self.next is not None:
             goals = await self.next.find_path(self.to_next(c, True), self.to_next(t, False))
 
-        logger.info({
+        yield False, {
             "layer": self.name,
             "selected": c,
             "choices": []
-        })
+        }
 
-        yield c
+        yield True, c
 
         while True:
             if await c.is_same_node(t):
@@ -108,49 +108,55 @@ class Layer:
 
             if self.next is not None:
                 try:
-                    g = await self.from_next(await anext(goals))
+                    while True:
+                        # pop the next goal
+                        is_result, result = await anext(goals)
+                        if is_result:
+                            yield False, result
+                            break
+                    g = await self.from_next(result)
                 except StopIteration:
-                    logger.info({
+                    yield False,{
                         "layer": self.name + 1,
                         "selected": t,
                         "choices": []
-                    })
+                    }
                     g = t
             else:
                 g = t
 
             while True:
                 if await c.is_same_node(t):
-                    # wait we found the true target!
+                    # we have reached the target
                     break
 
                 if await c.is_same_node(g):
+                    # we have reached the goal
                     c = g
                     break
 
                 count_steps = count_steps + 1
                 if count_steps >= hard_limit:
-                    logger.info({
+                    yield False, {
                         "layer": self.name,
                         "success": False,
-                    })
+                    }
                     raise RecursionError
-                    break
 
                 c, supplementary = await self.pincer_inference(c, g, pathway_bias, with_info=True)
                 c = await self.hippocampus.enhance(c)  # enhance signal preventing signal degradation
 
-                logger.info({
+                yield False, {
                     "layer": self.name,
                     "selected": c,
-                    "choices": [(self.hippocampus.enhance(r), p) for r, p in supplementary]
-                })
-                yield c
+                    "choices": supplementary
+                }
+                yield True, c
 
-        logger.info({
+        yield False, {
             "layer": self.name,
             "success": True,
-        })
+        }
 
     async def next_step(self, c: Node, t: Node, pathway_bias=0):
         # pathway_bias < 0 : use hippocampus
