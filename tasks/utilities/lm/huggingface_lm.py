93b849fee5ec1c720df9eec96fd0f010a94f754a
Patrick Virie <p.virie@gmail.com>
2024-09-17 15:58:28 +0700

    compute gradient with respect to future slots

--- tasks/utilities/lm/huggingface_lm.py
+++ tasks/utilities/lm/huggingface_lm.py
@@ -79,6 +79,7 @@ class Model(base.Model):
         # dot product with target embeddings to get logits
 
         # target_embeddings has shape (embedding_size,)
+        self.vocab_embedding = target_embeddings
 
         # get all token embeddings
         all_token_embeddings = model.get_input_embeddings().weight
@@ -90,6 +91,41 @@ class Model(base.Model):
 
     def reset_vocab_embedding_heuristic_score(self):
         self.vocab_embedding_heuristic_score = torch.zeros(model.config.vocab_size)
+
+
+    def compute_look_ahead_score(self, text:str, num_future_slots=16):
+
+        encoded_input = tokenizer(text, return_tensors='pt')
+
+        # append future slot to encoded_input
+        extended_input_ids = torch.cat(
+            [encoded_input['input_ids'], 
+             torch.zeros((1, num_future_slots), dtype=torch.long)], dim=-1
+        )
+
+        # apppend attention mask
+        extended_attention_mask = torch.cat(
+            [encoded_input['attention_mask'], 
+             torch.zeros((1, num_future_slots), dtype=torch.long)], dim=-1
+        )
+        
+        model_output = model(input_ids=extended_input_ids, attention_mask=extended_attention_mask, output_hidden_states=True)
+
+        # sum embedding of the future slots
+        future_slots_mean_embedding = model_output.hidden_states[-1][-num_future_slots:].mean(dim=1)
+        future_slots_mean_embedding_error = torch.mean((self.vocab_embedding - future_slots_mean_embedding[0])**2)
+ 
+        # embedding_hidden_states [batch, token_length, embedding_size]
+        embedding_hidden_states = model_output.hidden_states[0]
+        embedding_hidden_states.retain_grad()  # Ensure grads are retained
+        
+        # Backward pass
+        future_slots_mean_embedding_error.backward()
+
+        # The gradient will be with respect to the retained parts
+        grad = embedding_hidden_states.grad[0, num_future_slots]
+
+        print(grad)
 
 
     def get_best_next_token(self, text:str):
@@ -112,7 +148,7 @@ class Model(base.Model):
         encoded_input = tokenizer(text, return_tensors='pt')
         model_output = model(**encoded_input)
         logits = model_output.logits
-        logits = logits[0, -1, :] + 0.1 * self.vocab_embedding_heuristic_score
+        logits = logits[0, -1, :] + 0.2 * self.vocab_embedding_heuristic_score
         
         # select only top_k, keep the rest as -inf
         values, indices = torch.topk(logits, top_k)
@@ -132,39 +168,39 @@ if __name__ == "__main__":
 
     # m.reset_vocab_embedding_heuristic_score()
 
-    for i in range(10):
+    print()
+    print("============ Research ===============")
+    print()
 
-        print()
-        print("============ Research ===============")
-        print()
+    embedding = m.get_text_embedding("In general, do research before taking action. Make sure that everything is planned and calculated.")
+    m.precompute_vocab_embedding_heuristic_score(embedding)
 
-        embedding = m.get_text_embedding("In general, do research before taking action. Make sure that everything is planned and calculated.")
-        m.precompute_vocab_embedding_heuristic_score(embedding)
+    m.compute_look_ahead_score("To start a business, this is a guideline:")
 
-        input = "To start a business, this is a guideline:"
-        print(input)
-        for i in range(40):
-            next = m.sample_next_token(input)
-            print(next, end="")
-            input += next
+    # input = "To start a business, this is a guideline:"
+    # print(input)
+    # for i in range(40):
+    #     next = m.sample_next_token(input)
+    #     print(next, end="")
+    #     input += next
 
-        with open(os.path.join("/app/log", f"research_{str(i)}.txt"), "w") as f:
-            f.write(input)
+    # with open(os.path.join("/app/log", f"research.txt"), "a") as f:
+    #     f.write(input)
 
 
-        print()
-        print("============ Action ===============")
-        print()
+    # print()
+    # print("============ Action ===============")
+    # print()
 
-        embedding = m.get_text_embedding("Get to action first. Do not waste time on planning or research. Instead, learn from mistakes.")
-        m.precompute_vocab_embedding_heuristic_score(embedding)
+    # embedding = m.get_text_embedding("Get to action first. Do not waste time on planning or research. Instead, learn from mistakes.")
+    # m.precompute_vocab_embedding_heuristic_score(embedding)
 
-        input = "To start a business, this is a guideline:\n"
-        print(input)
-        for i in range(40):
-            next = m.sample_next_token(input)
-            print(next, end="")
-            input += next
-    
-        with open(os.path.join("/app/log", f"action_{str(i)}.txt"), "w") as f:
-            f.write(input)
+    # input = "To start a business, this is a guideline:\n"
+    # print(input)
+    # for i in range(40):
+    #     next = m.sample_next_token(input)
+    #     print(next, end="")
+    #     input += next
+
+    # with open(os.path.join("/app/log", f"action.txt"), "a") as f:
+    #     f.write(input)
