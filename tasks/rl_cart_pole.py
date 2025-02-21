af59f37ee640fde1808314f029a5f5a8f4305775
Patrick Virie <p.virie@gmail.com>
2025-02-22 04:35:16 +0700

    using normal gym to render

--- tasks/rl_cart_pole.py
+++ tasks/rl_cart_pole.py
@@ -7,13 +7,17 @@ from typing import List, Any
 from pydantic import BaseModel
 import argparse
 import sys
-import math
 import jax
 import jax.numpy as jnp
 import gymnax
-from gymnax.visualize import Visualizer
+import gym
+# gymnax visualizer is buggy
+# from gymnax.visualize import Visualizer
 
-from utilities.utilities import empty_directory
+import numpy as np
+np.bool8 = np.bool
+
+from utilities.utilities import empty_directory, write_gif
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
@@ -438,74 +442,52 @@ if __name__ == "__main__":
 
     with experiment_session(experiment_path) as context:
         
-        r_key = jax.random.key(random.randint(0, 1000))
+        env = gym.make("CartPole-v1", render_mode="rgb_array")
+        env.action_space.seed(random.randint(0, 1000))
 
-        env, env_params = gymnax.make("CartPole-v1")
-        jitted_step = jax.jit(env.step)
-        
-        def generate_visual(env, env_params, r_key, render_path, num_trials, action_method):
+        def generate_visual(render_path, num_trials, action_method):
             for j in range(num_trials):
-                r_key, key_reset = jax.random.split(r_key)
-                obs, state = env.reset(key_reset, env_params)
+                observation, info = env.reset(seed=random.randint(0, 1000))
                 output_gif = os.path.join(render_path, f"trial_{j}.gif")
-
-                state_seq = []
-                reward_seq = []
+                imgs = []
                 for _ in range(500):
-                    state_seq.append(state)
-                    r_key, r_step, r_action = jax.random.split(r_key, 3)
-                    action = action_method(obs, r_action)
-                    next_obs, state, reward, done, info = jitted_step(r_step, state, action, env_params)
-                    reward_seq.append(reward)
-                    if done:
+                    selected_action = action_method(observation)
+                    observation, reward, terminated, truncated, info = env.step(selected_action)
+                    img = env.render()
+                    imgs.append(img)
+                    if terminated or truncated:
                         break
-                    else:
-                        obs = next_obs
-
-                cum_rewards = jnp.cumsum(jnp.array(reward_seq))
-                vis = Visualizer(env, env_params, state_seq, cum_rewards)
-                vis.animate(output_gif)
-
+                write_gif(imgs, output_gif, fps=30)
 
         logging.info("----------------- Testing random behavior -----------------")
         logging.info(f"Baseline number of random steps: {context.average_random_steps}")
+        # result_path = os.path.join(experiment_path, "results", f"random")
+        # render_path = os.path.join(result_path, "render")
+        # os.makedirs(render_path, exist_ok=True)
+        # generate_visual(render_path, 5, lambda obs: env.action_space.sample())
 
-        result_path = os.path.join(experiment_path, "results", f"random")
-        render_path = os.path.join(result_path, "render")
-        os.makedirs(render_path, exist_ok=True)
-
-        def generation_random_action(observation, r_key):
-            return env.action_space(env_params).sample(r_key)
-    
-        generate_visual(env, env_params, r_key, render_path, 5, generation_random_action)
-
-        stable_state = alg.Expectation([1])
-        
         for i, parameter_set in enumerate(context.parameter_sets):
             logging.info(f"----------------- Testing model behavior {i} -----------------")
             result_path = os.path.join(experiment_path, "results", f"set_{i}")
 
             model = HUMN(**parameter_set)
+            stable_state = alg.Expectation([1])
 
-            def generation_action(observation, r_key):
-                a = model.react(alg.State(observation), stable_state)
-                selected_action = jnp.reshape(jnp.where(a.data > 0.5, 1, 0), ())
-                return selected_action
-            
             total_steps = 0
             num_trials = 100
-            print_steps = max(10, num_trials // 10)
+            print_steps = max(1, num_trials // 10)
             for j in range(num_trials):
                 if j % print_steps == 0 and j > 0:
+                    # print at every 10% progress
                     logging.info(f"Environment testing: {(j * 100 / num_trials):.2f}")
-                r_key, key_reset = jax.random.split(r_key)
-                obs, state = env.reset(key_reset, env_params)
+                observation, info = env.reset(seed=random.randint(0, 1000))
                 for _ in range(500):
-                    r_key, r_step, r_action = jax.random.split(r_key, 3)
-                    selected_action = generation_action(obs, r_action)
-                    next_obs, state, reward, done, info = jitted_step(r_step, state, selected_action, env_params)
+                    # selected_action = env.action_space.sample()
+                    a = model.react(alg.State(observation.data), stable_state)
+                    selected_action = 1 if np.asarray(a.data)[0].item() > 0.5 else 0
+                    observation, reward, terminated, truncated, info = env.step(selected_action)
                     total_steps += 1
-                    if done:
+                    if terminated or truncated:
                         break
 
             set_name = parameter_set["name"]
@@ -514,6 +496,10 @@ if __name__ == "__main__":
             render_path = os.path.join(result_path, "render")
             os.makedirs(render_path, exist_ok=True)
 
-            generate_visual(env, env_params, r_key, render_path, 5, generation_action)
+            def generation_action(observation):
+                a = model.react(alg.State(observation.data), stable_state)
+                return 1 if np.asarray(a.data)[0].item() > 0.5 else 0
+            
+            generate_visual(render_path, 5, generation_action)
 
         env.close()
